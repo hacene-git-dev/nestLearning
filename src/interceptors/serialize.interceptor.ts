0a3960a1cb4ce212ee1191e8852b0f4516112564
Hassen Oumerabet <hassen.oumerabet-extern@renault.com>
2025-07-03 16:46:03 +0200

    first commit

---
+++ src/interceptors/serialize.interceptor.ts
@@ -0,0 +1,27 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor, UseInterceptors } from "@nestjs/common";
+import { map, Observable } from "rxjs";
+import { plainToClass, plainToInstance } from "class-transformer"
+
+
+interface ClassConstructor {
+	new (...args: any[]): {};
+}
+
+export function Serialize(dto: ClassConstructor) {
+	return  UseInterceptors( new SerializeInterceptor(dto) )
+}
+
+export class SerializeInterceptor implements NestInterceptor {
+
+	constructor( private dto: ClassConstructor ) {}
+
+	intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+		return next.handle().pipe(
+			map((data: any) => {
+				return plainToClass(this.dto, data, {
+					excludeExtraneousValues: true,
+				});
+			}),
+		);
+	}
+}
---
+++ src/users/users.controller.ts
@@ -0,0 +1,59 @@
+import { Body, Controller, Post, Get, Patch, Param, Query, Delete, Session } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UsersService  } from './users.service';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from '../interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+@Serialize(UserDto)
+export class UsersController {
+
+	constructor( 
+		private userService: UsersService,
+		private authService: AuthService
+	) {}
+
+	@Post('signup')
+	createUser( @Body() body: CreateUserDto ){
+		return this.authService.signup( body.email, body.password );
+	}
+
+
+	@Post('signin')
+	signin( @Body() body: CreateUserDto ){
+		return this.authService.signin( body.email, body.password );
+	}
+
+	// @UseInterceptors( new SerializeInterceptor(UserDto) )
+	@Get('/:id')
+	findUser( @Param('id') id: string ) {
+		return this.userService.findOne(parseInt(id));
+	}
+
+	@Get()
+	findAllUsers( @Query('email') email: string ) {
+		return this.userService.find(email);
+	}
+
+	@Delete('/:id')
+	removeUser( @Param('id') id: string ) {
+		return this.userService.remove(parseInt(id));
+	}
+
+	@Patch('/:id')
+	updateUser( @Param('id') id: string, @Body() body: UpdateUserDto ) {
+		return this.userService.update(parseInt(id), body);
+	}
+
+	@Get('/colors/:color')
+	setColor(@Param('color') color: string, @Session() session: any) {
+		session.color = color;
+	}
+
+	@Get('/colors')
+	getColors(@Session() session: any) {
+		return	session.color;
+	}
+}
